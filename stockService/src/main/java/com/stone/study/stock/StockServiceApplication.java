69799820792b9267f77236abc680eb8578f36325
郑昌林 <zhengchagnlin@bmsoft.com>
2019-03-19 16:08:00 +0800

    feign，http调用

--- stockService/src/main/java/com/stone/study/stock/StockServiceApplication.java
+++ stockService/src/main/java/com/stone/study/stock/StockServiceApplication.java
@@ -1,4 +1,4 @@
-package com.stone.study.eurekaClient;
+package com.stone.study.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableFeignClients
-public class EurekaClientApplication {
+public class StockServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(EurekaClientApplication.class, args);
+    SpringApplication.run(StockServiceApplication.class, args);
   }
 
 }
